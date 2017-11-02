45c94c2f99338a874abd79ef1af1de30590bf133
Laith <laith@ovestlabs.com.au>
2017-11-02 18:41:34 +1100

    Added search request.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -4,11 +4,58 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            query: 'kittens'
+        };
+    }
+
+    parseUrls(data) {
+        const photoList = data.photo;
+        const photos = [];
+
+        for (var i = 0; i < photoList.length; i++) {
+            const item = photoList[i];
+            const url = `https://farm${item.farm}.static.flickr.com/${item.server}/${item.id}_${item.secret}.jpg`;
+
+            photos.push({
+                title: item.title,
+                url: url
+            });
+        }
+
+        console.log(photos);
+    }
+
+    handleQueryChange(e) {
+        this.state({
+            query: e.currentTarget.value
+        });
+    }
+
+    handleSearchClick(e) {
+        const query = this.state.query;
+
+        if (query.length < 2) {
+            return;
+        }
+
+        const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e7cc266ae2b0e0d78e279ce8e361736&format=json&nojsoncallback=1&safe_search=1&text=${query}`;
+
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            }).then((json) => {
+                this.parseUrls(json.photos);
+            });
     }
 
     render() {
         return (
-            <h1>Flickr Search</h1>
+            <div>
+                <input type="text" value={this.state.query} onChange={this.handleQueryChange.bind(this)}/>
+                <button onClick={this.handleSearchClick.bind(this)}>Search</button>
+            </div>
         );
     }
 };
